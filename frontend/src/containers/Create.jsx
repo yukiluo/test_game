e5b446f9e60f3351b0cfd780ec524ef659908588
mars <lkh1314678@gmail.com>
2022-04-25 02:17:41 +0800

    add create room views

---
+++ frontend/src/containers/Create.jsx
@@ -0,0 +1,129 @@
+import '../index.css'
+import{BrowserRouter, Routes, Route, Link, useNavigate} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client'
+import axios from 'axios';
+
+
+const frameStyle = {
+    width:"40%",
+    height:"500px",
+    margin:"50px auto",
+    // backgroundColor:"palegreen",
+}
+const divFlexStyle = {
+    display: "flex",
+    justifyContent: "center", 
+    alignItems:"center",
+    // borderWidth:"1px"
+    // marginTop: "20px",
+}
+const lebelStyle = {
+    width:"20%",
+    textAlign:"center"
+}
+const inputStyle = {
+    width:"200px",
+    height:"40px",
+    margin:"10px 10px",
+    fontSize:"20px",
+    textAlign:"center",
+    boxSizing:"content-box",
+    // backgroundColor:"palegreen",
+}
+const buttonStyle = {
+    width:"150px",
+    height:"40px",
+    margin:"40px 10px",
+    boxSizing:"content-box",
+    bottom: "10px",
+    cursor: "pointer",
+    // backgroundColor:"palegreen",
+}
+
+const socket = io(process.env.REACT_APP_SOCKETIO_URL);
+// socket.on("changeLineWidth", function(value){
+//     // console.log("======")
+//     console.log(value)
+// })
+
+
+const Create = ({setCurrentRoomId, setRoomInfo}) =>{
+    const [mode, setMode] = useState("general");
+    const [theme, setTheme] = useState("general");
+    const [playerCount, setPlayerCount] = useState(5);
+    const [score, setScore] = useState(70);
+
+    // const navigate = useNavigate();
+    // async function createRoom(){
+    //     // socket.emit("createRoom", {mode, theme, playerCount, score})
+        
+    //     let resData = await axios.post("http://localhost:3000/api/1.0/", {mode, theme, playerCount, score})
+    //     let roomInfo = resData.data; 
+    //     setCurrentRoomId(roomInfo.roomId);
+    //     setRoomInfo(roomInfo);
+
+    //     navigate(`/game/${roomInfo.roomId}`)
+    // }
+
+    // useEffect(() => {
+    //     console.log("changeSetting")
+    // },[mode, theme, playerCount, score])
+
+    return (
+        <div style={frameStyle} className="frame_border" >
+            <div style={{textAlign:"center", fontSize:"40px", margin:"30px auto 30px"}}>配置</div>
+            
+            <div style={divFlexStyle}>
+                <label style={lebelStyle}>遊戲模式</label>
+                <select disabled style={inputStyle} className="component_border" onChange={(e)=>setMode(e.target.value)}>
+                    <option disabled>請選擇模式</option>
+                    <option value="general">普通模式</option>
+                    <option value="double" disabled>雙人模式</option>
+                </select>
+            </div>
+            <div style={divFlexStyle}>
+                <label style={lebelStyle}>主題</label>
+                <select disabled style={inputStyle} className="component_border" onChange={(e)=>setTheme(e.target.value)}>
+                    <option disabled>請選擇主題</option>
+                    <option value="general">綜合</option>
+                    <option value="animal" disabled>動物</option>
+                    <option value="food" disabled>食物</option>
+                </select>
+            </div>
+            <div style={divFlexStyle}>
+                <label style={lebelStyle}>人數上限</label>
+                <select  style={inputStyle} className="component_border" onChange={(e)=>setPlayerCount(e.target.value)}>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                </select>
+            </div>
+            <div style={divFlexStyle}>
+                <label style={lebelStyle}>目標分數</label>
+                <select  style={inputStyle} className="component_border" onChange={(e)=>setScore(e.target.value)}>
+                    <option value="70">70</option>
+                    <option value="100">100</option>
+                    <option value="120">120</option>
+                    <option value="150">150</option>
+                    <option value="180">180</option>
+                    <option value="200">200</option>
+                    <option value="250">250</option>
+                </select>
+            </div>
+
+            <div style={divFlexStyle}>
+                {/* <Link to={"/game/"+"abc"} > */}
+                    <button onClick={createRoom} className="component_border" style={buttonStyle}>開始遊戲</button>
+                {/* </Link> */}
+            </div>
+            
+        </div>
+    )
+}
+
+export default Create;
